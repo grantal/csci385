4fb8d7fa106b6902cfe299762b24fa153c32e0eb
grantal <AlexV348@gmail.com>
2017-10-04 11:39:44 -0700

    splat gets camera info

--- hw4/website/include/splat.js
+++ hw4/website/include/splat.js
@@ -1,4 +1,4 @@
-function ExportSplat(eye, plane, right, up, away) {
+function ExportSplat(cameraMove) {
   /*
      function to make the postscript file
      the function name begins with "Export" because of my eslint settings
@@ -6,22 +6,17 @@ function ExportSplat(eye, plane, right, up, away) {
   // read in the file that was loaded
   const file = document.getElementById('file').files[0];
   if (file) {
-
     // make sure filename ends in .obj
     const regexobj = /\w*\.obj/;
     if (regexobj.test(file.name)) {
       const r = new FileReader();
       r.onload = function fileLoaded(e) {
         console.log(e.target.result);
-        console.log(eye);
-        console.log(plane);
-        console.log(right);
-        console.log(up);
-        console.log(away);
+        console.log(cameraMove);
       };
       r.readAsText(file);
     } else {
-      alert("Needs to be an obj file!");
+      alert('Needs to be an obj file!');
     }
   }
 }
