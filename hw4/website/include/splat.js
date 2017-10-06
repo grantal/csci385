7c9c36218e6bab2e3b9bf289f970b1b33a80e2e5
grantal <AlexV348@gmail.com>
2017-10-06 12:25:34 -0700

    added download function

--- hw4/website/include/splat.js
+++ hw4/website/include/splat.js
@@ -1,3 +1,21 @@
+function download(filename, text) {
+  /*
+  This makes the browser download a text file with filename 'filename' and contents 'text'
+  I stole this function from here:
+  https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+  */
+  const element = document.createElement('a');
+  element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`);
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
 function ExportSplat(cameraMove) {
   /*
      function to make the postscript file
@@ -11,12 +29,14 @@ function ExportSplat(cameraMove) {
     if (regexobj.test(file.name)) {
       const r = new FileReader();
       r.onload = function fileLoaded(e) {
-        console.log(e.target.result);
-        console.log(cameraMove);
+        //console.log(e.target.result);
+        console.log(cameraMove.eye);
+        console.log(cameraMove.center);
+        console.log(cameraMove.up);
+        
+        download("hello.txt","This is the content of my file :)");
       };
       r.readAsText(file);
-    } else {
-      alert('Needs to be an obj file!');
     }
   }
 }
