04bdb55a6ec5d1938f5d52a8260101c21650313c
grantal <AlexV348@gmail.com>
2017-10-11 11:02:56 -0700

    commiting what I have

--- hw4/website/include/splat.js
+++ hw4/website/include/splat.js
@@ -23,7 +23,8 @@ function vertexSplat(vertex, cameraMove) {
   used this:
   http://people.wku.edu/qi.li/teaching/446/cg4_geometry.pdf
   */
-  return JSM.Project(vertex, cameraMove.eye, cameraMove.center, cameraMove.up, cameraMove.fieldOfView, 1, cameraMove.nearClippingPlane, cameraMove.farClippingPlane, [0,0,100,100]);
+  const result = JSM.Project(vertex, cameraMove.eye, cameraMove.center, cameraMove.up, cameraMove.fieldOfView, 1, cameraMove.nearClippingPlane, cameraMove.farClippingPlane, [0,0,100,100]);
+  return result;
 }
 
 function intersect(v1, u1, v2, u2) {
@@ -77,6 +78,93 @@ function intersect(v1, u1, v2, u2) {
   return new JSM.Coord2D(x, y);
 }
 
+function distance(a, b) {
+  /*
+    Computes the 2D distance from a to b
+  */
+  const X = a.x - b.x;
+  const Y = a.y - b.y;
+  return Math.sqrt((X * X) + (Y * Y));
+}
+
+function distanceComparatorGenerator(Origin) {
+  /*
+    returns a function that will compare two arguments
+    and will say that the lower argument is the one closer to Origin
+  */
+  function distanceComparator(a, b) {
+    const adist = distance(Origin, a);
+    const bdist = distance(Origin, b);
+    if (adist < bdist) {
+      return -1;
+    }
+    if (bdist < adist) {
+      return 1;
+    }
+    return 0;
+  }
+  return distanceComparator;
+}
+
+function sortUnique(arr, compareFunction) {
+  /*
+    sorts list and removes any duplicates. I stole this function from here:
+    https://stackoverflow.com/questions/4833651/javascript-array-sort-and-unique
+  */
+  if (arr.length === 0) return arr;
+
+  arr.sort(compareFunction);
+  const ret = [arr[0]];
+  for (let i = 1; i < arr.length; i += 1) { // start loop at 1 as element 0 can never be a duplicate
+    if (compareFunction(arr[i - 1], arr[i]) !== 0) {
+      ret.push(arr[i]);
+    }
+  }
+  return ret;
+}
+
+function midpoint(a, b) {
+  /*
+  returns the 2D midpoint of a and b
+  */
+  return new JSM.Coord2D((a.x + b.x) / 2, (a.y + b.y) / 2);
+}
+
+function findRay(cameraMove, m) {
+  /*
+    takes a point (m) in 2D and the camera information and returns the 3D unit vector that points
+    from the camera in the direction of m on the near clipping plane 
+  */
+  let M = new JSM.Coord(m.x, m.y, 1);
+  M = JSM.Unproject(M, cameraMove.eye, cameraMove.center, cameraMove.up, cameraMove.fieldOfView, 1, cameraMove.nearClippingPlane, cameraMove.farClippingPlane, [0,0,100,100]);
+  console.log(M);
+  const dhat = JSM.CoordSub(M, cameraMove.eye);
+  dhat.Normalize();
+  return dhat;
+}
+
+function rayHitFace(R, d, Q1, Q2, Q3) {
+  /*
+    check to see if a ray coming from R in the direction of unit vector d intersects the face
+    defined by the points Q1, Q2, Q3
+  */
+  const v2 = JSM.CoordSub(Q2, Q1);
+  const v3 = JSM.CoordSub(Q3, Q1);
+  const n = JSM.VectorCross(v2, v3);
+  n.MultiplyScalar(1 / (v2.Length() * v3.Length));
+  const delta = JSM.VectorDot(JSM.CoordSub(R, Q1), n);
+  const nhat = new JSM.Coord(n.x, n.y, n.z);
+  nhat.MultiplyScalar(Math.sign(d) / nhat.Length());
+  const negd = new JSM.Coord(-1 * d.x, -1 * d.y, -1 * d.z);
+  const ro = JSM.VectorDot(negd, nhat);
+  if (ro < 0) {
+    return false;
+  }
+  const theta = Math.abs(delta) / ro; 
+  const P = JSM.CoordAdd(R, d.MultiplyScalar(theta));
+
+}
+
 
 // beginning and end of postscript file
 const header = '%!PS-Adobe-2.0\n/poly { 4 dict\nbegin\n/N exch def\n/A 360 N div def\n1 0 moveto\nN {\nA cos A sin lineto\n/A A 360 N div add def\n} repeat\nclosepath\nend\n} def\ngsave\n0 72 11 8.5 sub mul translate\n1 5 div setlinewidth\n5.0 5.0 scale\n';
@@ -95,16 +183,14 @@ function ExportSplat(cameraMove) {
     if (regexobj.test(file.name)) {
       const r = new FileReader();
       r.onload = function fileLoaded(e) {
-        // console.log(e.target.result);
-        console.log(cameraMove.eye);
-        console.log(cameraMove.center);
-        console.log(cameraMove.up);
 
         let psfile = header;
         // load in vertices and faces from obj file
         const vertices = [];
         const faces = [];
         const lines = e.target.result.split('\n');
+        // mapping from one endpoint on a line segment to all of the intersections on that segment
+        const segments = new Map();
         for (let i = 0; i < lines.length; i += 1) {
           // split line by whitespace
           const line = lines[i].split(/\s+/g);
@@ -117,10 +203,11 @@ function ExportSplat(cameraMove) {
           if (line[0] === 'f') {
             const face = [Number(line[1]), Number(line[2]), Number(line[3])];
             // this will loop through each pair of vertices on the face and draw a line
-            console.log(face);
             for (let j = 0; j < 3; j += 1) {
               const v = vertices[face[j] - 1];
               const u = vertices[face[((j + 1) % 3)] - 1];
+              // so it the key is 31, thats the fourth face, second edge
+              segments.set((faces.length * 10) + j,[v,[u]]); 
               psfile += '0.0 setgray\nnewpath\n';
               psfile += `${v.x} ${v.y} moveto\n`;
               psfile += `${u.x} ${u.y} lineto\n`;
@@ -131,10 +218,16 @@ function ExportSplat(cameraMove) {
                 for (let h = 0; h < 3; h += 1) {
                   const v2 = vertices[faces[k][h] - 1];
                   const u2 = vertices[faces[k][((h + 1) % 3)] - 1];
-                  // makes a circle at the intersection
-                  const result = intersect(v, u, v2, u2);
-                  if (result) {
-                    psfile += `${result.x} ${result.y} 0.7 0 360 arc closepath\n0.0 setgray fill\n`;
+                  // make sure these edges are not the same
+                  if (!((v.IsEqual(v2) && u.IsEqual(u2)) || (v.IsEqual(u2) && u.IsEqual(v2)))){
+                    // makes a circle at the intersection
+                    const result = intersect(v, u, v2, u2);
+                    if (result) {
+                      //psfile += `${result.x} ${result.y} 0.7 0 360 arc closepath\n0.0 setgray fill\n`;
+                      // add this intersection to the dict entry for each line segment it's on
+                      segments.get((faces.length * 10) + j)[1].push(result);
+                      segments.get((k * 10) + h)[1].push(result);
+                    }
                   }
                 }
               }
@@ -143,9 +236,33 @@ function ExportSplat(cameraMove) {
           }
         }
 
-        console.log(vertices);
+        // here we add all of the midpoints
+        for (const tuple of segments) {
+          // first, we order the instersections on the line by their distance from v
+          const v = tuple[1][0];
+          let intersections = tuple[1][1];
+          const comp = distanceComparatorGenerator(v);
+          intersections = sortUnique(intersections, comp);
 
+          // color the dots
+          const colorVector = new JSM.Coord(v.x, v.y, v.z);
+          colorVector.Normalize();
+          colorVector.z = Math.random();
 
+          // add each midpoint of each intersection on the segment
+          for (let i = 0; i < intersections.length; i += 1) {
+            let result = 0;
+            if (i === 0) {
+              result = midpoint(v, intersections[i]);
+            } else {
+              result = midpoint(intersections[i - 1], intersections[i]);
+            }
+            console.log(findRay(cameraMove, result));
+            psfile += `${result.x} ${result.y} 0.7 0 360 arc closepath\n${colorVector.x} ${colorVector.y} ${colorVector.z} setrgbcolor fill\n`;
+          }
+        }
+
+        // add the footer and download the file
         psfile += footer;
         const psfilename = file.name.replace(/\.obj/g, '.ps');
         download(psfilename, psfile);
